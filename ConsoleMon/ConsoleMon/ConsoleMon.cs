829b9a7172ba050e9e287a79095ad9ce1b1aa3c6
Davey van Wijk <30650@ma-web.nl>
2022-05-09 10:43:58 +0200

    first commit

---
+++ ConsoleMon/ConsoleMon/ConsoleMon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMon
+{
+    internal class ConsoleMon
+    {
+        int health;
+        int energy;
+        string name;
+
+        internal ConsoleMon(int health, int energy, string name)
+        {
+            this.health = health;
+            this.energy = energy;
+            this.name = name;
+        }
+
+        internal void TakeDamage(int damage)
+        {
+            health -= damage;
+        }
+        internal void DepleteEnergy(int energy)
+        {
+            this.energy -= energy;
+        }
+    }
+}
---
+++ ConsoleMon/ConsoleMon/Skill.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMon
+{
+    internal class Skill
+    {
+        int damage;
+        int energyCost;
+        string name;
+
+        internal Skill(int damage, int energyCost, string name)
+        {
+            this.damage = damage;
+            this.energyCost = energyCost;
+            this.name = name;
+        }
+        internal void UseOn(ConsoleMon target, ConsoleMon caster)
+        {
+            target.TakeDamage(damage);
+            caster.DepleteEnergy(energyCost);
+        }
+    }
+}
